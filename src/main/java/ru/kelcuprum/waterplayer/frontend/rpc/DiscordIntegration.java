5628fb97bf44b26ae08d10695f1c2e3f5335fa04
Kel <me@kelcuprum.ru>
2024-08-25 14:18:02 +0300

    Added button for Discord

--- src/main/java/ru/kelcuprum/waterplayer/frontend/rpc/DiscordIntegration.java
+++ src/main/java/ru/kelcuprum/waterplayer/frontend/rpc/DiscordIntegration.java
@@ -1,5 +1,6 @@
 package ru.kelcuprum.waterplayer.frontend.rpc;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.jagrosh.discordipc.IPCClient;
 import com.jagrosh.discordipc.IPCListener;
@@ -9,9 +10,13 @@ import com.jagrosh.discordipc.entities.RichPresence;
 import com.jagrosh.discordipc.entities.User;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import org.apache.logging.log4j.Level;
 import ru.kelcuprum.waterplayer.WaterPlayer;
 import ru.kelcuprum.waterplayer.frontend.localization.MusicHelper;
+
+import java.util.Objects;
 
 public class DiscordIntegration {
     public static IPCClient client;
@@ -92,14 +97,23 @@ public class DiscordIntegration {
         else {
             RichPresence.Builder builder = new RichPresence.Builder().setActivityType(ActivityType.Listening);
             String icon = MusicHelper.isFile() ? "file" : (track.getInfo().artworkUrl == null || track.getInfo().artworkUrl.isBlank()) ? "no_icon" : track.getInfo().artworkUrl;
-            builder.setLargeImage(icon, Component.translatable("waterplayer.track.service", MusicHelper.getServiceName(MusicHelper.getService(track))).getString())
+            builder.setLargeImage(icon, MusicHelper.getServiceName(MusicHelper.getService(track)).getString())
                     .setDetails(MusicHelper.getTitle())
                     .setState(MusicHelper.getAuthor());
+            getYonKaGorMoment(track, builder, icon);
             long start = System.currentTimeMillis() - MusicHelper.getPosition();
             if(WaterPlayer.player.getAudioPlayer().isPaused()) builder.setSmallImage("paused");
             else {
                 builder.setStartTimestamp(parseSeconds(start));
                 if(!MusicHelper.getIsLive()) builder.setEndTimestamp(parseSeconds(start+MusicHelper.getDuration()));
+            }
+            if(track.getInfo().uri.startsWith("https://") || track.getInfo().uri.startsWith("http://")){
+                JsonArray buttons = new JsonArray();
+                JsonObject button = new JsonObject();
+                button.addProperty("label", MusicHelper.getServiceName(MusicHelper.getService(track)).getString());
+                button.addProperty("url", track.getInfo().uri);
+                buttons.add(button);
+                builder.setButtons(buttons);
             }
             send(builder.build());
         }
@@ -123,5 +137,30 @@ public class DiscordIntegration {
                 WaterPlayer.log(ex.getMessage() == null ? ex.getClass().getName() : ex.getMessage(), Level.ERROR);
             }
         }
+    }
+    protected void getYonKaGorMoment(AudioTrack track, RichPresence.Builder builder, String icon) {
+        if (!MusicHelper.getAuthor(track).equals("YonKaGor")) return;
+        switch (MusicHelper.getTitle(track)) {
+//            case "I Forgot That You Exist", "I Forgot That You Exist. ¯\\_(ツ)_/¯" -> Items.MUSIC_DISC_WAIT;
+            case "Top 10 Things to Do Before You Die", "Top 10 Things To Do Before You Die",
+                 "[TW] Top 10 Things To Do Before You Die (Censored)" -> {
+                builder.setLargeImage("https://wf.kelcu.ru/mods/waterplayer/icons/seadrive.gif", "HowTo");
+            }
+//            case "Trash Talkin'", "kennyoung & YonKaGor - Trash Talkin'" -> Items.MUSIC_DISC_OTHERSIDE;
+//            case "Fallacy" -> Items.MUSIC_DISC_PIGSTEP;
+//            case "You're Just Like Pop Music" -> Items.MUSIC_DISC_MELLOHI;
+//            case "Dandelion", "Dandelion \uD83C\uDF3C (Full Song)" -> Items.DANDELION;
+//            case "Mr. Sunfish", "Good Morning, Mr. Sunfish!", "Fish ! (Original)" -> Items.TROPICAL_FISH;
+//            case "You'll Be Gone" -> Items.MUSIC_DISC_MALL;
+//            case "It's Normal", "It's Normal [TW]" -> Items.MUSIC_DISC_11;
+//            case "Circus Hop", "Circus Hop [TW]" -> Items.MUSIC_DISC_CHIRP;
+//            case "Paper Alibis", "Paper Alibis (Full Song)" -> Items.PAPER;
+//            case "Silly Plans" -> Items.LIGHT_BLUE_BED;
+//            case "Silly Plans ~ Revisit" -> Items.FILLED_MAP;
+//            case "Another Mistake" -> Items.BARRIER;
+//            case "Memory Merge" -> Items.FLINT_AND_STEEL;
+//            case "Waterland", "Waterland (Lyric video)" -> Items.WATER_BUCKET;
+//            case "Artificial Abandonment", "(Original Song) Artificial Abandonment" -> Items.MOSSY_COBBLESTONE;
+        };
     }
 }
